24b970c3aadbb3182d9c6044b62d17074e82ee2d
carlos <carlos0camacho@gmail.com>
2020-08-08 16:04:30 -0500

    test passed

--- src/functions.js
+++ src/functions.js
@@ -1,5 +1,15 @@
 const fizzbuzz = (num)=>{
-    return num.toString();
+    if(num%3 ===0 || num%5 ===0 ){
+        if(num%3 + num%5 === 0){
+            return "fizzbuzz";            
+        }else if(num%3 ===0){
+            return "fizz";    
+        }else{
+            return "buzz";
+        }
+    }else{
+        return "nofiz"
+    }
 }
 
 
--- test/test.FB.js
+++ test/test.FB.js
@@ -3,9 +3,7 @@ const {fizzbuzz} = require('../src/functions')
 
 
 describe('FizBuzz',()=>{
-    it('Shuld return a number as a string',()=>{
-        expect(fizzbuzz(3)).to.eql('3');
-    });
+
     it('Shuld return fizz for 3*x',()=>{
         expect(fizzbuzz(21)).to.eql('fizz');
     });
