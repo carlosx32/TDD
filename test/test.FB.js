903c6684c202ae8dbe3fd0d254c373d10039ef76
carlos <carlos0camacho@gmail.com>
2020-08-08 15:45:49 -0500

    new modules

--- test/test.FB.js
+++ test/test.FB.js
@@ -6,5 +6,11 @@ describe('FizBuzz',()=>{
     it('Shuld return a number as a string',()=>{
         expect(fizzbuzz(3)).to.eql('3');
     });
+    it('Shuld return fizz for 3*x',()=>{
+        expect(fizzbuzz(3)).to.eql('fizz');
+    });
+    it('Shuld return fizz for 3*x',()=>{
+        expect(fizzbuzz(3)).to.eql('buzz');
+    });
 
 });
