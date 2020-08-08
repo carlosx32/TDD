4802b4395c10f7b24ae0ed86374181e00ec0a8b4
carlos <carlos0camacho@gmail.com>
2020-08-08 16:06:09 -0500

    test defined corectly

--- test/test.FB.js
+++ test/test.FB.js
@@ -7,7 +7,7 @@ describe('FizBuzz',()=>{
     it('Shuld return fizz for 3*x',()=>{
         expect(fizzbuzz(21)).to.eql('fizz');
     });
-    it('Shuld return buzz for 3*x',()=>{
+    it('Shuld return buzz for 5*x',()=>{
         expect(fizzbuzz(10)).to.eql('buzz');
     });
     it('Shuld return fizzbuzz for 5*3*x',()=>{
